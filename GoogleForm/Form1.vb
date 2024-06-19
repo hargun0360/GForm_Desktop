320710ba5931a6e9f776507d1313c102a5d30865
hargun0360 <hargunsingh0720@gmail.com>
2024-06-19 15:42:38 +0530

    Add: View Submission form UI

--- GoogleForm/Form1.vb
+++ GoogleForm/Form1.vb
@@ -2,4 +2,13 @@
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
+
+    Private Sub BtnViewSubmissions_Click(sender As Object, e As EventArgs) Handles BtnViewSubmissions.Click
+
+    End Sub
+
+    Private Sub BtnCreateSubmission_Click(sender As Object, e As EventArgs) Handles BtnCreateSubmission.Click
+
+    End Sub
+
 End Class
